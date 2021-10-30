7138fbaf6628b5a9d1593f67cb7db134ddad68f7
andreeazgarcea <andreeazgarcea31@gmail.com>
2021-10-30 10:10:55 +0300

    Initial commit

---
+++ curs/src/tema2.java
@@ -0,0 +1,18 @@
+public class tema2 {
+
+    public static void main (String [] args) {
+
+System.out.println("hello Andreea");
+        public static void main(String [] args)
+        System.out.println(2 + 3);
+       public int sum(){
+           int result =2+3;
+           return result;
+
+           public static int memberVariable=3;
+            public static int substract(2,3);
+
+        }
+
+    }
+}
